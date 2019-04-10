ace339bb090d712f556282a42fe4e98828e0cb92
Patrick Kim <pkim050@gmail.com>
2019-04-10 10:56:20 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,14 +1,45 @@
 class TweetsController < ApplicationController
   get '/tweets/new' do
+    if session[:user_id] != nil
+      erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
 
+  post '/tweets/new' do
+    redirect '/tweets/new' if session[:user_id] != nil
+    redirect '/login'
+  end
+
+  get '/tweets' do
+    #binding.pry
+    if session[:user_id] != nil
+      @user = current_user
+      erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
 
   post '/tweets' do
-
+    @user = User.find(session[:user_id])
+    redirect '/tweets/new' if params[:tweet][:content].empty?
+    @user.tweets << Tweet.new(params[:tweet])
+    redirect "/users/#{@user.slug}"
   end
 
   get '/tweets/:id/edit' do
 
+  end
+
+  get '/tweets/:id' do
+    if session[:user_id] != nil
+      @tweet = Tweet.find(params[:user_id])
+      erb :'/tweets/show'
+    else
+      redirect '/login'
+    end
   end
 
   post '/tweets/:id' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,49 @@
 class UsersController < ApplicationController
   get '/signup' do
+    redirect '/tweets' if session[:user_id] != nil
     erb :'/users/signup'
   end
 
   post '/signup' do
-    #binding.pry
     redirect '/signup' if params[:username].empty? || params[:email].empty? || params[:password].empty?
-    binding.pry
-    @user = User.new(params)
-    session[:user_id] = @user.id
-    redirect '/tweets'
+    @user = User.create(params)
+    #binding.pry
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
   end
 
-  get '/users/login' do
-    erb :'/users/login'
+  get '/login' do
+    #binding.pry
+    if session[:user_id] != nil
+      @user = current_user
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
   end
 
-  post '/users/login' do
-
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    #binding.pry
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
   end
 
-  get '/users/logout' do
+  get '/logout' do
+    session.clear if session[:user_id] != nil
+    redirect '/login'
+  end
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
   end
 end
